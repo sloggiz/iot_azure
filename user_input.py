0282f4ffdc059c5738924d218cc2c939c035a745
sloggiz <sloggz@gmail.com>
2023-05-10 09:42:42 +0200

    initial repo commit

---
+++ user_input.py
@@ -0,0 +1,14 @@
+"""
+===================================================================================================
+Author:      Stepan Gaponiuk - D ONE 2023
+Description: This script contains user input for streaming Raspberry Pi reed signal data into Azure
+===================================================================================================
+"""
+
+# GPIO
+LED_PIN = 4  # GPIO pin where the LED is connected to
+SWITCH_PIN = 18  # GPIO pin where the reed switch is connected to
+
+# Azure
+IOT_HUB_CON_STR = "YOUR IOT HUB CONNECTION STRING"  # Paste your Azure IoT hub connection string
+DEVICE_IDPIC = "YOUR TOPIC NAME HERE"  # Paste your Azure IoT hub device name
